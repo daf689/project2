de19eb55ea4fcc864ddb8ce47c262a2b4cb6b1b2
daf689 <117328418+daf689@users.noreply.github.com>
2022-12-05 18:17:53 -0500

    Add files via upload

---
+++ Script4.js
@@ -0,0 +1,47 @@
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+toggle.addEventListener('click', () =>
+    nav.classList.toggle('active'))
+
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if(activeSlide > slides.length - 1){
+        activeSlide = 0;
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+
+    if(activeSlide < 0){
+        activeSlide = slides.length - 1
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+setBgToBody()
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => slide.classList.remove('active'))
+
+    slides[activeSlide].classList.add('active')
+}
+
